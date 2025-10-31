e366fe9ce6e29b1b6be13b8dd054e2bf9dbe43e8
Andrii <andrii@asymptotic.tech>
2025-10-31 16:54:59 +0200

    wip

--- external-crates/move/crates/move-compiler/src/parser/attributes.rs
+++ external-crates/move/crates/move-compiler/src/parser/attributes.rs
@@ -744,6 +744,7 @@ fn parse_spec_only_parametized(context: &mut Context, loc: &Loc, inner_attrs: &S
                             };
                             target = Some(access.clone());
                         } else if key.value == KA::VerificationAttribute::LOOP_INV_LABEL_NAME.into() {
+                            println!("Val value: {:?}", val.value);
                             match val.value {
                                 AttributeValue_::Value(sp!(_, P::Value_::Num(n))) => {
                                     label = n.as_usize();
